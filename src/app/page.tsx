730565709a189c7ddddb72f9efd25b69072f6aff
Aman Verma <amanverma0428@gmail.com>
2025-03-29 15:15:19 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,10 +40,10 @@ export default function Home() {
 		<>
 			<div className="grid place-content-center lg:pt-56 md:pt-48 pt-44 px-4 lg:px-0">
 				<div className="space-y-2 px-10">
-					<h1 className="flex flex-col lg:flex-row md:flex-row">
+					<div>
 						<SplitText
-							text="Hello!"
-							className="text-8xl font-semibold text-cyan -ml-2 "
+							text="<Hello World!/>"
+							className="lg:text-8xl md:test-5xl text-4xl font-semibold text-cyan"
 							delay={150}
 							animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
 							animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
@@ -51,21 +51,12 @@ export default function Home() {
 							threshold={0.2}
 							rootMargin="-50px"
 						/>
-						<SplitText
-							text="I'm Aman"
-							className="lg:text-8xl text-6xl font-semibold -ml-2"
-							delay={150}
-							animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
-							animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
-							easing="easeOutCubic"
-							threshold={0.2}
-							rootMargin="-50px"
-						/>
-					</h1>
-					<div className="lg:text-3xl">
+					</div>
+					<div className="lg:text-2xl">
 						<div className="flex flex-wrap">
+							<BlurText text="I'm Aman. A full stack developer, " animateBy="letters" delay={20} direction="bottom" />
 							<BlurText
-								text="A full stack developer with expertise in "
+								text="Expertise in "
 								animateBy="letters"
 								delay={20}
 								direction="bottom"
